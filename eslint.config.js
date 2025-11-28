c16242d8d56a2fa324266c80fdf86312c0f7100d
alihamza-321 <alihamza-321@brainium-tech.com>
2025-11-29 03:01:23 +0500

    added automation-testing

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import noCommentedCode from 'eslint-plugin-no-commented-code'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.vite', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +19,33 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'no-commented-code': noCommentedCode,
+    },
+    rules: {
+      // Unused variables and parameters
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off', // Turn off base rule as it conflicts with @typescript-eslint version
+      
+      // Detect commented code
+      'no-commented-code/no-commented-code': 'warn',
+      
+      // Warn about warning comments (TODO, FIXME, etc.)
+      'no-warning-comments': [
+        'warn',
+        {
+          terms: ['todo', 'fixme', 'xxx', 'hack'],
+          location: 'anywhere',
+        },
+      ],
     },
   },
 ])
